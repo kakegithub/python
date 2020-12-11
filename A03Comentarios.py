bde7e76bd2736f4405a1d0bb99a439024d2ab326
kakegithub <74675012+kakegithub@users.noreply.github.com>
2020-12-11 11:50:21 +0100

    .

---
+++ A03Comentarios.py
@@ -0,0 +1,11 @@
+# Escribir comentarios en python
+
+# Esto es una comentario de una linea
+
+
+'''Esto es un comentario de 
+    varias lineas.'''
+
+""" Esto tambien es un comentario
+de varias lineas"""
+
