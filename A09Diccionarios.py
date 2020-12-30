31fe78037e3697c28c3acfbb3db0d695dbee6cd6
kakegithub <74675012+kakegithub@users.noreply.github.com>
2020-12-30 11:17:59 +0100

    .

--- A09Diccionarios.py
+++ A09Diccionarios.py
@@ -98,6 +98,64 @@ print("Los lenguajes mencionados son:")
 for lenguaje in set(lenguajes_favoritos.values()):
     print(lenguaje.title())
 
+# Lista de Diccionarios
+alien_0={'color':'green', 'points':5}
+alien_1={'color':'yellow', 'points':10}
+alien_2={'color':'red', 'points':15}
+
+# Metemos en una lista todos los diccionarios
+aliens=[alien_0,alien_1,alien_2]
+
+# Recorremos la lista
+for alien in aliens:
+    print(alien)
+
+# Creamos una lista vacia para guardar los aliens
+aliens = []
+
+# Creamos 30 aliens
+for alien_number in range(30):
+    new_alien={'color':'green', 'points':5, 'speed':'slow'}
+    aliens.append(new_alien)
+
+# Mostramos los 5 primeros aliens
+for alien in aliens[:5]:
+    print(alien)
+print("........")
+
+# Mostramos todos los aliens creados
+print("Total aliens:"+str(len(aliens)))
+
+# Modificando aliens
+for alien in aliens[0:3]:
+    if alien['color']=='green':
+        alien['color']='yellow'
+        alien['speed']='medium'
+        alien['points']=10
+
+# Mostramos los 5 primeros aliens
+for alien in aliens[0:5]:
+    print(alien)
+    print(alien)
+print("........")
+
+# Un diccionario en un diccionario
+usuarios= {
+    'aenstein':{'nombre':'Arbert', 'apellidos':'einstein', 'lugar de nacimiento':'Princeton'},
+    'mcurie':{'nombre':'Marie', 'apellidos':'Curie', 'lugar de nacimiento':'Paris'},
+}
+
+for usuario, informacion_usuario in usuarios.items():
+    print("\nUsuario:" + usuario)
+    nombre_completo = informacion_usuario['nombre'] + " " + informacion_usuario['apellidos']
+    localidad = informacion_usuario['lugar de nacimiento']
+
+    print("\nNombre completo:" + nombre_completo.title())
+    print("\nLocalidad:" + localidad.title())
+        
+
+
+
 
 
 
